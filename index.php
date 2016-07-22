49fd8c230447d1be6ede53a194ba24623bf7a59d
syahrulutomo <irullvandalis@gmail.com>
2016-07-22 13:35:06 +0700

    prepare statement

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
+    //atribut atribut yg diperlukan
     $server = 'localhost';
     $user = 'root';
     $pass = '';
     $db_name = 'pdo_tuts';
     
+    //koneksi ke DB
     try 
     {    
         $conn = new PDO("mysql:host=$server;dbname=$db_name", $user, $pass);
@@ -14,11 +16,37 @@
         echo $exc->getMessage();
     }
     
-    $result = $conn->query("SELECT * FROM users");
     
-    while($row = $result->fetch())
+    //menampilkan data
+    $result = $conn->query("SELECT * FROM users");
+    $total = $result->rowCount();
+    
+    
+    while($row = $result->fetch(PDO::FETCH_OBJ))
     {
-        echo $row['username'] . '<br>';
+        echo $row->username .'  ' .$row->password . '<br>';
     }
-	
+    
+    echo 'Total : ' .$total;
+    
+    
+    //prepared statement
+    $name = 'irullls';
+    $password = '123463';
+    
+    $sql = "INSERT INTO users(username,password) VALUES(:name,:pass)";
+    $query = $conn->prepare($sql);
+    
+    $params =array(
+                    ':name'=>$name,
+                    ':pass'=>$password,
+                  );
+    $params2 =array(
+                    ':name'=>'sagejava',
+                    ':pass'=>'007',
+                  );
+    
+    $query->execute(params);
+    $query->execute(params2); 
+    
 ?>
